df5a87699f008170a8655302774bf832f374eab2
vineet <vnjain2004@gmail.com>
2024-03-01 15:15:58 +0530

    second commit

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@
 #![no_main]
 use core::panic::PanicInfo;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    loop{}
-}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop{}
 }
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop{}
+}
 
+//instead of using cargo build, use cargo build --target wasm32-unknown-unknown
+//if that gives an error, rustup target add wasm32-unknown-unknow first.
