a678aa960f686fb8b0bc98024162e242b38a8111
shashank <shashank45jain@gmail.com>
2024-03-02 12:46:30 +0530

    post 2 fixes

---
+++ readme.txt
@@ -0,0 +1,1 @@
+post 2 - use xargo instead cargo or use cargo build -Z build-std=core --target x86_64-os_in_rust.json
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 #![no_std]
 #![no_main]
-use core::panic::PanicInfo;
+#![cfg(not(test))]
 
+use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop{}
+    loop {}
 }
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop{}
+    loop {}
 }
 
 //instead of using cargo build, use cargo build --target wasm32-unknown-unknown
