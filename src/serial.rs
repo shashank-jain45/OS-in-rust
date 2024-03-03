2fbfcc7b49a1311ca0831f68899db5af32ae1741
vineet <vnjain2004@gmail.com>
2024-03-03 18:43:22 +0530

    bare bones done:)

---
+++ src/serial.rs
@@ -0,0 +1,37 @@
+//This module is for the serial port implementation which has to be used instead of the TCP model to run.
+
+
+
+use uart_16550::SerialPort;
+use spin::Mutex;
+use lazy_static::lazy_static;
+
+lazy_static!{
+    pub static ref SERIAL1: Mutex<SerialPort>={
+        let mut serial_port=unsafe{SerialPort::new(0x3F8)};
+        serial_port.init();
+        Mutex::new(serial_port)
+    };
+}
+//0x3F8 is the standard port for first serail interface.
+//uart uses multiple i/o ports.
+#[doc(hidden)]
+pub fn _print(args: ::core::fmt::Arguments){
+    use core::fmt::Write;
+    SERIAL1.lock().write_fmt(args).expect("Printing to serial failed");
+}
+
+#[macro_export]
+macro_rules! serial_print{
+    ($($arg:tt)*) =>{
+        $crate::serial::_print(format_args!($($arg)*));
+    };
+}
+
+#[macro_export]
+macro_rules! serial_println{
+    ()=>($crate::serial_print!("\n"));
+    ($fmt:expr)=>($crate::serial_print!(concat!($fmt,"\n")));
+    ($fmt:expr,$($arg:tt)*)=>($crate::serial_print!(
+        concat!($fmt,"\n"),$($arg)*));
+}
--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -2,30 +2,23 @@
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(os_in_rust::test_runner)]
-#![reexport_test_harness_main="test_main"]
-
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 use os_in_rust::println;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}","!");
-    #[cfg(test)]
     test_main();
+
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}",info);
-    loop{}
+fn test_println(tests: &[&dyn Fn()]) {
+    println!("test_println output");
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     os_in_rust::test_panic_handler(info)
 }
-
