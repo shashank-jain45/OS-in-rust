2d40291d3d1a2b6385f6fb571e36a45ebf34ce88
vineet <vnjain2004@gmail.com>
2024-03-03 12:40:57 +0530

    blog3 updated

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,21 @@
 #![no_std]
 #![no_main]
-#![cfg(not(test))]
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}","!");
+    loop {}
+}
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}",info);
     loop{}
 }
-// static HELLO: &[u8] = b"Hello World!";
-mod vga_buffer;
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    // let vga_buffer = 0xb8000 as *mut u8;
 
-    // for (i, &byte) in HELLO.iter().enumerate() {
-    //     unsafe {
-    //         *vga_buffer.offset(i as isize * 2) = byte;
-    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xf;
-    //    }
-    // }
-    // vga_buffer::print_something();
-    // use core::fmt::Write;
-    // vga_buffer::WRITER.lock().write_str("Hello Again").unwrap();
-    // write!(vga_buffer::WRITER.lock(),", same numbers in same order:{} {}",42,1.0/3.0).unwrap();
-    println!(" Hello World{}","!");
-    loop {}
-}
+
 
 //instead of using cargo build, use cargo build --target wasm32-unknown-unknown
 //if that gives an error, rustup target add wasm32-unknown-unknown first.
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -6,7 +6,7 @@ use spin::Mutex;
 lazy_static! {
     pub static ref WRITER: Mutex<Writer>=Mutex::new(Writer {
         column_position:0,
-        color_code: ColorCode::new(Color::Magenta, Color::Black),
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
     });
 }
@@ -99,7 +99,7 @@ impl Writer {
     }
     fn new_line(&mut self){
         for row in 1..BUFFER_HEIGHT{
-            for col in 1..BUFFER_WIDTH{
+            for col in 0..BUFFER_WIDTH{
                 let character=self.buffer.chars[row][col].read();
                 self.buffer.chars[row-1][col].write(character);
             }
@@ -127,7 +127,7 @@ pub fn print_something(){
     use core::fmt::Write;
     let mut writer = Writer{
         column_position:0,
-        color_code:ColorCode::new(Color::Magenta, Color::Black),
+        color_code:ColorCode::new(Color::Yellow, Color::Black),
         buffer: unsafe { &mut*(0xb8000 as *mut Buffer)},
     };
     writer.write_byte(b'H');
